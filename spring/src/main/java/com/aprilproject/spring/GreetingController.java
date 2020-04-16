0e66a2ea986fe29ab81899dd6447f63166e67b29
unknown <sarah.akerfeldt@frontit.se>
2020-04-16 11:33:29 +0200

    created controller class Greeting

---
+++ spring/src/main/java/com/aprilproject/spring/GreetingController.java
@@ -0,0 +1,16 @@
+package com.aprilproject.spring;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class GreetingController {
+
+    @GetMapping("/index") 
+    public String greeting(@RequestParam(name="nameInURL", required = false, defaultValue = "World!") String name, Model model) {
+        model.addAttribute("nameInModel", name);
+        return "index";
+    }
+}
