ed79e7509bff58440edabe216e2683753379a8ec
Andreas <andreas@Andreas-MacBook-Pro-2018.local>
2020-04-21 13:40:31 +0200

    Updated customer page

--- spring/src/main/java/com/aprilproject/spring/CustomerController.java
+++ spring/src/main/java/com/aprilproject/spring/CustomerController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -19,23 +20,32 @@ public class CustomerController {
     public String customerForm(Model model) {
         model.addAttribute("customer", new Customer());
        // List<Customer> list = repository.findAll();
-        model.addAttribute("list", repository.findAll());
+       setCustomerModel(model);
         return "customer";
     }
 
     @PostMapping("/customer")
     public String customerSubmit(@ModelAttribute Customer customer, Model model) {
         repository.save(customer);
-       // List<Customer> list = repository.findAll();
-        model.addAttribute("list", repository.findAll());
+        setCustomerModel(model);
+        return "customer";
+    }
+
+    @DeleteMapping("/customerDelete")
+    public String deleteCustomer(@ModelAttribute Customer customer, Model model) {
+        repository.deleteById(customer.id);
+        setCustomerModel(model);
         return "customer";
     }
 
     @GetMapping("/allCustomers")
     public String getCustomers(Model model) {
-        List<Customer> list = repository.findAll();
-        model.addAttribute("list", list);
+        setCustomerModel(model);
         return "allCustomers";
+     }
+
+     private void setCustomerModel(Model model) {
+        model.addAttribute("list", repository.findAll());
      }
 
 }
